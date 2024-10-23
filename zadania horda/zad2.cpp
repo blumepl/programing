49836cf8d8daad061cb3f990369dbdcb06f0ab31
blumepl <ittebksz@gmail.com>
2024-10-23 13:15:29 +0200

    Komentarz

---
+++ zadania horda/zad2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a = 5;
+	int b = 3;
+	int c = a + b;
+	int d = a - b;
+	int e = a * b;
+	cout << "Liczba: " << a << endl;
+	cout << "Liczba: " << b << endl;
+	cout << "Liczba: " << c << endl;
+	cout << "Liczba: " << d << endl;
+	cout << "Liczba: " << e << endl;
+}

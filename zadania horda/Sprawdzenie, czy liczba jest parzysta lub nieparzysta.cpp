8f9a4a91c15a3f202ed39f4064dd4ee1c276edd0
blumepl <ittebksz@gmail.com>
2025-04-01 11:05:23 +0200

    Komentarz

---
+++ zadania horda/Sprawdzenie, czy liczba jest parzysta lub nieparzysta.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+bool funkcja(int a) {if(a % 2 = 0) return true;
+else( a % 2 = 0) return false;
+}
+int main(){
+	cin << a;
+	bool wynik = funkcja(a);
+	
+	if(wynik == true) {
+		cout << "liczba parzysta";
+		else {
+			cout << "liczba nieparzysta"
+			}
+					}
+}

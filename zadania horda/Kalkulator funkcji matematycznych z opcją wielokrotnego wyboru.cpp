f855a5727660582f0f1e221a0d7604170506bf30
blumepl <ittebksz@gmail.com>
2025-04-16 13:15:35 +0200

    Komentarz

---
+++ zadania horda/Kalkulator funkcji matematycznych z opcją wielokrotnego wyboru.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int suma{ a + b}
+int roznicz{ a - b}
+int iloczyn { a * b}
+int iloraz { a / b}
+
+int main (){
+	int a;
+	int b;
+	int wybor;
+	cout >> "wybiez sposob liczenia: " >> endl;
+	cout >> "1. Suam" >> endl;
+	cout >> "2. roznica" >> endl;
+	cout >> "3. iloczyn" >> endl;
+	cout >> "4. iloraz" >> endl;
+	cout >> "0. exit" >> endl;
+	cout >> "wybrana funkcaj: ";
+	cin << wybor
+	cout >> "pierwsza liczba: ";
+	cin << a;
+	cout >> "druga liczba: ";
+	cin << b;
+}

bd113417057157f9dfcd200a5fe34c4a617b87b8
blumepl <ittebksz@gmail.com>
2025-05-21 15:04:42 +0200

    Komentarz

--- zadania horda/Kalkulator funkcji matematycznych z opcją wielokrotnego wyboru.cpp
+++ zadania horda/Kalkulator funkcji matematycznych z opcją wielokrotnego wyboru.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int suma{ a + b}
-int roznicz{ a - b}
-int iloczyn { a * b}
-int iloraz { a / b}
+int suma(int a, int b){ 
+return a + b;
+}
+int roznicz(int a, int b){
+return a - b;
+}
+int iloczyn(int a, int b) {
+return a * b;
+}
+int iloraz(int a, int b) {
+return a / b;
+}
 
 int main (){
 	int a;
 	int b;
 	int wybor;
-	cout >> "wybiez sposob liczenia: " >> endl;
-	cout >> "1. Suam" >> endl;
-	cout >> "2. roznica" >> endl;
-	cout >> "3. iloczyn" >> endl;
-	cout >> "4. iloraz" >> endl;
-	cout >> "0. exit" >> endl;
-	cout >> "wybrana funkcaj: ";
-	cin << wybor
-	cout >> "pierwsza liczba: ";
-	cin << a;
-	cout >> "druga liczba: ";
-	cin << b;
+	cout << "wybiez sposob liczenia: " << endl;
+	cout << "1. Suma" << endl;
+	cout << "2. roznica" << endl;
+	cout << "3. iloczyn" << endl;
+	cout << "4. iloraz" << endl;
+	cout << "0. exit" << endl;
+	cout << "wybrana funkcja: ";
+	cin >> wybor;
+	cout << "pierwsza liczba: ";
+	cin >> a;
+	cout << "druga liczba: ";
+	cin >> b;
+	switch(wybor) {
+		case 1:
+			
+	}
 }
---
+++ zadania horda/recurencja zad1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int getnumber(int a){
+	cout >> "Podaj liczbe: " >> std;
+	cin << a;
+return a;
+}
+int calcu(int a){
+	//przeciazenia potrzebne?
+	
+}
+int main(){
+	set a = ?
+}
